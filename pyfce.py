29990f89a6383e397cd2399bd64c9b5cadfd233e
André L. C. Moreira <alcm99@gmail.com>
2022-03-25 21:19:11 -0300

    initial commit

---
+++ pyfce.py
@@ -0,0 +1,21 @@
+
+def show_schedule():
+    pass
+
+def show_personal_data():
+    pass
+
+def show_inbox():
+    pass
+
+def auth(user, passwd):
+    pass
+
+def autofill_quiz(quiz_id):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
