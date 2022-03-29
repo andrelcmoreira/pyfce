cafd4343b778c0c939daf3887ceb079dfc98e03f
André L. C. Moreira <alcm99@gmail.com>
2022-03-28 23:56:01 -0300

    - adding requirements file - adding auth command

--- pyfce.py
+++ pyfce.py
@@ -1,24 +1,64 @@
+from argparse import ArgumentParser
+from sys import argv, exit
 
-def show_schedule():
-    pass
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
-def show_personal_data():
-    pass
+def do_auth():
+    user = input('type the user: ')
+    passwd = input('type the password: ')
 
-def show_inbox():
-    pass
+    ret = auth(user, passwd)
+
+    if ret:
+        print('the credentials are valid')
+    else:
+        print('failed to login with the provided credentials')
 
 def auth(user, passwd):
-    pass
+    op = ChromeOptions()
+    op.add_argument('headless')
 
-def autofill_quiz(quiz_id):
-    pass
+    drv = Chrome(options=op)
 
-def has_pending_quiz():
-    pass
+    drv.get('https://qacademico.ifce.edu.br/qacademico/index.asp?t=1001')
+    drv.find_element(by=By.XPATH, value='//*[@id="txtLogin"]').send_keys(user)
+    drv.find_element(by=By.XPATH, value='//*[@id="txtSenha"]').send_keys(passwd)
+    drv.find_element(by=By.XPATH, value='//*[@id="btnOk"]').click()
 
-def main():
-    pass
+    try:
+        drv.find_element(by=By.XPATH, value='//*[@id="Image1"]')
+
+        return True
+    except NoSuchElementException:
+        return False
+
+def parse_args():
+    parser = ArgumentParser(prog='pyfce')
+
+    parser.add_argument(
+        '-c',
+        '--check-credentials',
+        help='check the provided credentials',
+        action='store_true'
+    )
+
+    # no arguments were provided
+    if len(argv) == 1:
+        parser.print_help()
+
+    return parser.parse_args()
+
+def main(args):
+    if args.check_credentials:
+        do_auth()
 
 if __name__ == "__main__":
-    main()
+    try:
+        args = parse_args()
+
+        main(args)
+    except Exception as err:
+        print(err)
+        exit(1)
